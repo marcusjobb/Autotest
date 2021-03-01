cb797f89e9427efa83e4e988a8ff1f10476c7c91
Marcus Medina <medina.jobb@outlook.com>
2021-03-01 13:19:25 +0100

    First

---
+++ Autotest/AutotestTests/InformationTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Autotest.Tests
+{
+    [TestClass()]
+    public class InformationTests
+    {
+        [TestMethod()]
+        public void GetNameTest()
+        {
+            var info = new Information();
+            Assert.AreEqual("Marcus", info.GetName());
+        }
+    }
+}
